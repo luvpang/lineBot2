f69496aa688c4efe1fe49f97bfda31998b59ab8d
luvpang <51397479+luvpang@users.noreply.github.com>
2019-08-06 19:50:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def callback():
     userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
     if (userText == 'สวัสดี') :
-        sendText(user,'ไอ้เจ้าบ้า')
+        sendText(user,'ดีจ้า')
     elif (userText == 'น่ารัก') :
         sendText(user,'แน่นอนอยู่แล้ว')
     else :
